7efb489a5262470d52b18ab4ba7e1cc9b9c90ae0
Thamidu-Nadun <nadunrz101@gmail.com>
2025-11-28 10:30:00 +0530

    feat: update UserReqDto.java

---
+++ src/main/java/com/nadun/blog/dto/request/UserReqDto.java
@@ -0,0 +1,14 @@
+package com.nadun.blog.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserReqDto {
+    private String name;
+    private String email;
+    private String password;
+}
