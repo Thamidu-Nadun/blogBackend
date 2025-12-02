f79cfab887c3bd5a564f253a552079386a2b4152
Thamidu-Nadun <nadunrz101@gmail.com>
2025-12-03 01:27:48 +0530

    Created: webstory-service

---
+++ src/main/java/com/nadun/blog/service/WebStoryService.java
@@ -0,0 +1,12 @@
+package com.nadun.blog.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nadun.blog.repo.WebStoryRepo;
+
+@Service
+public class WebStoryService {
+    @Autowired
+    private WebStoryRepo webStoryRepo;
+}
