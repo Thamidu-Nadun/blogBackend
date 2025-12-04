8b8b68ae24166e72d3dec968f8f325f291856f3a
Thamidu-Nadun <nadunrz101@gmail.com>
2025-12-04 23:56:41 +0530

    Updated Tags.java

--- src/main/java/com/nadun/blog/model/Tags.java
+++ src/main/java/com/nadun/blog/model/Tags.java
@@ -3,8 +3,12 @@ package com.nadun.blog.model;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import com.nadun.blog.model.content.Content;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,10 +27,12 @@ public class Tags {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(unique = true, nullable = false)
     private String name;
 
     @ManyToMany
     @JsonBackReference
+    @JsonIgnore
     @JoinTable(name = "content_tags", joinColumns = @jakarta.persistence.JoinColumn(name = "tag_id"), inverseJoinColumns = @jakarta.persistence.JoinColumn(name = "content_id"))
     private List<Content> contents;
 }
