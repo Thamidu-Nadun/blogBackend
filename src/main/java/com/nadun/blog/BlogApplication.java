77567d15e3defe5a9ff27b07d48221577b2e7114
Thamidu-Nadun <nadunrz101@gmail.com>
2025-12-04 23:56:38 +0530

    Updated BlogApplication.java

--- src/main/java/com/nadun/blog/BlogApplication.java
+++ src/main/java/com/nadun/blog/BlogApplication.java
@@ -1,7 +1,9 @@
 package com.nadun.blog;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BlogApplication {
@@ -9,6 +11,11 @@ public class BlogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 		System.out.println("Blog Application Started Successfully!");
+	}
+
+	@Bean
+	ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
